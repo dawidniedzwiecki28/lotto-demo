f0cb5f7b57c6ce4fc5323598571f4ebc4fa64fbd
Dawid Niedźwiecki <dawidniedzwiecki28@gmail.com>
2022-08-03 11:30:52 +0200

    Initial commit

---
+++ src/main/java/com/niedzwiadek28code/lottodemo/LottoDemoApplication.java
@@ -0,0 +1,9 @@
+package com.niedzwiadek28code.lottodemo;
+
+public class LottoDemoApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Lotto Player");
+    }
+
+}
---
+++ src/test/java/com/niedzwiadek28code/lottodemo/LottoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niedzwiadek28code.lottodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottoDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
