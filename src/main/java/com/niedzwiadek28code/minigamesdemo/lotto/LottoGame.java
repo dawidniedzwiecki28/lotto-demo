3d95f133fd8be24423173fb84f6244c37e7a8c0f
Dawid Niedźwiecki <dawidniedzwiecki28@gmail.com>
2022-08-04 13:50:42 +0200

    more refactoring

--- src/main/java/com/niedzwiadek28code/minigamesdemo/lotto/LottoGame.java
+++ src/main/java/com/niedzwiadek28code/minigamesdemo/lotto/LottoGame.java
@@ -10,20 +10,20 @@ public class LottoGame implements Game {
     public void play() {
 
         try {
-            // Display introduction message
+                // Display introduction message
             System.out.println(LottoMessages.GAME_INTRODUCTION);
             Thread.sleep(1500);
             System.out.println(LottoMessages.START_MESSAGE);
             Thread.sleep(2000);
 
-            // Get number from user
+                // Get number from user
             lottoService.getNumbersFromUser();
 
-            // Display user numbers and winning numbers
+                // Display user numbers and winning numbers
             System.out.println("\nYour numbers: " + lottoService.getUserNumbers());
             System.out.println("Winning numbers: " + lottoService.getGeneratedNumbers());
 
-            // Display game result
+                // Display game result
             System.out.println();
             System.out.printf(LottoMessages.DISPLAY_RESULT, lottoService.getGameResult());
 
--- src/main/java/com/niedzwiadek28code/minigamesdemo/lotto/LottoService.java
+++ src/main/java/com/niedzwiadek28code/minigamesdemo/lotto/LottoService.java
@@ -16,13 +16,13 @@ public class LottoService {
     }
 
     public int getGameResult(){
-        int result = 0;
+        int score = 0;
 
         for (Integer number : userNumbers){
             if(generatedNumbers.contains(number))
-                result++;
+                score++;
         }
-        return result;
+        return score;
     }
 
     public Set<Integer> generateNumbers(){
@@ -40,7 +40,7 @@ public class LottoService {
             System.out.printf(LottoMessages.NEXT_NUMBER_MESSAGE, userNumbers.size()+1);
 
             Integer userNumber;
-            userNumber = getNumber(scanner);
+            userNumber = getIntereger(scanner);
 
             if(isNotNumberFromRange(userNumber)) {
                 System.out.printf(LottoMessages.NUMBER_OUT_RANGE, LottoConfig.LOWER_RANGE, LottoConfig.UPPER_RANGE);
@@ -54,12 +54,12 @@ public class LottoService {
         }
         scanner.close();
     }
-    private Integer getNumber(Scanner scan) {
-        while (!scan.hasNextInt()) {
-            scan.next();
+    private Integer getIntereger(Scanner scanner) {
+        while (!scanner.hasNextInt()) {
+            scanner.next();
             System.out.printf(LottoMessages.NOT_INTEGER, userNumbers.size()+1);
         }
-        return scan.nextInt();
+        return scanner.nextInt();
     }
     private boolean isNotNumberFromRange(int number){
         return number < LottoConfig.LOWER_RANGE || number > LottoConfig.UPPER_RANGE;
