59cb52616c96bd8ad6058dc6796ee65551903826
Dawid Niedźwiecki <dawidniedzwiecki28@gmail.com>
2022-08-08 10:44:44 +0200

    RandomNumbersGeneratorTest update

--- src/test/java/com/niedzwiadek28code/minigamesdemo/lotto/RandomNumbersGeneratorTest.java
+++ src/test/java/com/niedzwiadek28code/minigamesdemo/lotto/RandomNumbersGeneratorTest.java
@@ -1,26 +1,68 @@
 package com.niedzwiadek28code.minigamesdemo.lotto;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Set;
+import java.util.stream.Stream;
 
 import static com.niedzwiadek28code.minigamesdemo.lotto.RandomNumbersGenerator.generateRandomNumbers;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RandomNumbersGeneratorTest {
 
-    @Test
-    void should_generate_Random_Numbers(){
+    @ParameterizedTest
+    @MethodSource("expectedNumberOfRandomNumbersDataProvider")
+    public void shouldReturnExpectedNumberOfRandomNumbers(int numberPool, int minNumber, int maxNumber) {
+            // given
 
-        // given
-        int numberPool = 6, minNumber = 2, maxNumber = 10;
-
-        // when
-        Set<Integer> numbers = generateRandomNumbers(numberPool,minNumber,maxNumber);
-
-        // then
+            // when
+        Set<Integer> numbers = generateRandomNumbers(numberPool, minNumber, maxNumber);
+            // then
         assertEquals(numbers.size(), numberPool);
-        assertFalse(numbers.contains(minNumber-1));
-        assertFalse(numbers.contains(maxNumber+1));
+    }
+    private static Stream<Arguments> expectedNumberOfRandomNumbersDataProvider(){
+        return Stream.of(
+                Arguments.of(5,1,9),
+                Arguments.of(0,1,7),
+                Arguments.of(1,1,1),
+                Arguments.of(95,1,99)
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("rangeOfRandomNumbersProvider")
+    public void shouldReturnTrueWhenRandomNumberIsNotLowerThanMinValue(int minNumber, int maxNumber){
+            // given
+        int numberPool = 1;
+            // when
+        Set<Integer> numbers = generateRandomNumbers(numberPool, minNumber, maxNumber);
+        int temp = numbers.iterator().next();
+            // then
+        for(int i=0; i<100; i++) {
+            assertFalse(temp < minNumber);
+        }
+    }
+    @ParameterizedTest
+    @MethodSource("rangeOfRandomNumbersProvider")
+    public void shouldReturnTrueWhenRandomNumberIsNotBiggerThanMaxValue(int minNumber, int maxNumber){
+            // given
+        int numberPool = 1;
+            // when
+        Set<Integer> numbers = generateRandomNumbers(numberPool, minNumber, maxNumber);
+        int temp = numbers.iterator().next();
+            // then
+        for(int i=0; i<100; i++) {
+            assertFalse(temp > maxNumber);
+        }
+    }
+    private static Stream<Arguments> rangeOfRandomNumbersProvider(){
+        return Stream.of(
+                Arguments.of(0,1),
+                Arguments.of(1,1),
+                Arguments.of(-6,2),
+                Arguments.of(99,100),
+                Arguments.of(-7,-5)
+        );
     }
 }
