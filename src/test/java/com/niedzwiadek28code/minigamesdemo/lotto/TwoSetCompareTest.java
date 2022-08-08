459bb608fb8cb0c75460885d78ca1e063a2f63d6
Dawid Niedźwiecki <dawidniedzwiecki28@gmail.com>
2022-08-08 13:45:39 +0200

    Add two unit test class, TwoSetCompareTest and UserNumbersRetrieverTest

---
+++ src/test/java/com/niedzwiadek28code/minigamesdemo/lotto/TwoSetCompareTest.java
@@ -0,0 +1,44 @@
+package com.niedzwiadek28code.minigamesdemo.lotto;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoSetCompareTest {
+
+    @ParameterizedTest
+    @MethodSource("dataProvider")
+    public void shouldReturnHowManyNumbersAreTheSameInTwoSetCollection(int expectedResult, Set<Integer> winningNumbers, Set<Integer> userNumbers){
+            //given
+
+            //when
+        int result = TwoSetCompare.howManyNumbersContainInBothSet(
+                winningNumbers,
+                userNumbers);
+            //then
+        assertEquals(expectedResult,result);
+    }
+    private static Stream<Arguments> dataProvider(){
+        return Stream.of(
+                Arguments.of(
+                        0,
+                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                        new HashSet<>(Arrays.asList(7, 11, 33, 14, 25, 76))),
+                Arguments.of(
+                        1,
+                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                        new HashSet<>(Arrays.asList(0, 5, 33, 14, 11, 8))),
+                Arguments.of(
+                        6,
+                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)),
+                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6)))
+        );
+    }
+}
---
+++ src/test/java/com/niedzwiadek28code/minigamesdemo/lotto/UserNumbersRetrieverTest.java
@@ -0,0 +1,48 @@
+package com.niedzwiadek28code.minigamesdemo.lotto;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserNumbersRetrieverTest {
+
+    @ParameterizedTest
+    @MethodSource("userInputValueProvider")
+    public void shouldReturnTrueForCorrectUserInputValue(int howManyNumbers, String numbers){
+            // given
+        UserNumbersRetriever userNumbersRetriever =
+                new UserNumbersRetriever(mockScannerIn(numbers));
+            // when
+        Set<Integer> numbersFromUser = userNumbersRetriever.getNumbersFromUser(
+                howManyNumbers,
+                1,
+                99);
+            // then
+        assertEquals(numbersFromUser.size(), howManyNumbers);
+    }
+    private static Stream<Arguments> userInputValueProvider(){
+        return Stream.of(
+                Arguments.of(0,""),
+                Arguments.of(1,"4 0"),
+                Arguments.of(6,"1 2 3 4 5 6"),
+                Arguments.of(3,"1 2 -3 k 5 -6"),
+                Arguments.of(2,"4 0 8 65 82 16"),
+                Arguments.of(7,"1 mm 3 -4 5 6 100 8 0 10 98")
+        );
+    }
+    private Scanner mockScannerIn(String data) {
+        InputStream stdin = System.in;
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        Scanner scanner = new Scanner(System.in);
+        System.setIn(stdin);
+        return scanner;
+    }
+}
