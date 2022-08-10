2c6b70aa2002ee494b5c290b2cdbb47851ecd8a0
Dawid Niedźwiecki <dawidniedzwiecki28@gmail.com>
2022-08-10 15:48:36 +0200

    Refactor Test class

--- src/test/java/com/niedzwiadek28code/minigamesdemo/game/lotto/lotto/RandomNumbersGeneratorTest.java
+++ src/test/java/com/niedzwiadek28code/minigamesdemo/game/lotto/lotto/RandomNumbersGeneratorTest.java
@@ -18,57 +18,65 @@ class RandomNumbersGeneratorTest {
     @ParameterizedTest
     @MethodSource("expectedNumberOfRandomNumbersDataProvider")
     public void should_Fail_When_NumberOfRandomNumbersDifferentThanExpected(int numberPool, int minNumber, int maxNumber) {
-            // given
+        // given
+        Set<Integer> numbers;
 
-            // when
-        Set<Integer> numbers = generator.generateRandomNumbers(numberPool, minNumber, maxNumber);
-            // then
+        // when
+        numbers = generator.generateRandomNumbers(numberPool, minNumber, maxNumber);
+
+        // then
         assertEquals(numbers.size(), numberPool);
     }
-    private static Stream<Arguments> expectedNumberOfRandomNumbersDataProvider(){
+
+    private static Stream<Arguments> expectedNumberOfRandomNumbersDataProvider() {
         return Stream.of(
-                Arguments.of(5,1,9),
-                Arguments.of(0,1,7),
-                Arguments.of(1,1,1),
-                Arguments.of(95,1,99)
+                Arguments.of(5, 1, 9),
+                Arguments.of(0, 1, 7),
+                Arguments.of(1, 1, 1),
+                Arguments.of(95, 1, 99)
         );
     }
+
     @ParameterizedTest
     @MethodSource("rangeOfRandomNumbersProvider")
-    public void should_Fail_When_RandomNumberIsBiggerThanMinValue(int minNumber, int maxNumber){
-            // given
+    public void should_Fail_When_RandomNumberIsBiggerThanMinValue(int minNumber, int maxNumber) {
+        // given
         int numberPool = 1;
+        for (int i = 0; i < 100; i++) {
 
-        for(int i=0; i<100; i++) {
             // when
             Set<Integer> numbers = generator.generateRandomNumbers(numberPool, minNumber, maxNumber);
             int temp = numbers.iterator().next();
+
             // then
             assertFalse(temp < minNumber);
         }
     }
+
     @ParameterizedTest
     @MethodSource("rangeOfRandomNumbersProvider")
-    public void should_Fail_When_RandomNumberIsLowerThanMaxValue(int minNumber, int maxNumber){
-            // given
+    public void should_Fail_When_RandomNumberIsLowerThanMaxValue(int minNumber, int maxNumber) {
+        // given
         int numberPool = 1;
+        for (int i = 0; i < 100; i++) {
 
-        for(int i=0; i<100; i++) {
             // when
             Set<Integer> numbers = generator.generateRandomNumbers(numberPool, minNumber, maxNumber);
             int temp = numbers.iterator().next();
+
             // then
             assertFalse(temp > maxNumber);
         }
     }
-    private static Stream<Arguments> rangeOfRandomNumbersProvider(){
+
+    private static Stream<Arguments> rangeOfRandomNumbersProvider() {
         return Stream.of(
-                Arguments.of(0,1),
-                Arguments.of(1,1),
-                Arguments.of(-6,2),
-                Arguments.of(99,100),
-                Arguments.of(1,99),
-                Arguments.of(-7,-5)
+                Arguments.of(0, 1),
+                Arguments.of(1, 1),
+                Arguments.of(-6, 2),
+                Arguments.of(99, 100),
+                Arguments.of(1, 99),
+                Arguments.of(-7, -5)
         );
     }
 }
--- src/test/java/com/niedzwiadek28code/minigamesdemo/game/lotto/lotto/UserNumbersRetrieverTest.java
+++ src/test/java/com/niedzwiadek28code/minigamesdemo/game/lotto/lotto/UserNumbersRetrieverTest.java
@@ -17,29 +17,33 @@ class UserNumbersRetrieverTest {
 
     @ParameterizedTest
     @MethodSource("userInputValueProvider")
-    public void should_FailGettingNumbersFromUser_When_UserInputIntegersLessThanExpectedPool(int howManyNumbers, String numbers){
-            // given
+    public void should_FailGettingNumbersFromUser_When_UserInputIntegersLessThanExpectedPool(int howManyNumbers, String numbers) {
+        // given
         UserNumbersRetriever userNumbersRetriever =
                 new UserNumbersRetriever();
-            // when
+
+        // when
         Set<Integer> numbersFromUser = userNumbersRetriever.getNumbersFromUser(
                 mockScannerIn(numbers),
                 howManyNumbers,
                 1,
                 99);
-            // then
+
+        // then
         assertEquals(numbersFromUser.size(), howManyNumbers);
     }
-    private static Stream<Arguments> userInputValueProvider(){
+
+    private static Stream<Arguments> userInputValueProvider() {
         return Stream.of(
-                Arguments.of(0,""),
-                Arguments.of(1,"4 0"),
-                Arguments.of(6,"1 2 3 4 5 6"),
-                Arguments.of(3,"1 2 -3 k 5 -6"),
-                Arguments.of(2,"4 0 8 65 82 16"),
-                Arguments.of(7,"1 mm 3 -4 5 6 100 8 0 10 98")
+                Arguments.of(0, ""),
+                Arguments.of(1, "4 0"),
+                Arguments.of(6, "1 2 3 4 5 6"),
+                Arguments.of(3, "1 2 -3 k 5 -6"),
+                Arguments.of(2, "4 0 8 65 82 16"),
+                Arguments.of(7, "1 mm 3 -4 5 6 100 8 0 10 98")
         );
     }
+
     private Scanner mockScannerIn(String data) {
         InputStream stdin = System.in;
         System.setIn(new ByteArrayInputStream(data.getBytes()));
